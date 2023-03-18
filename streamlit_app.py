34c598caf3762bbce422cb57cc9bf81f7ec80863
kmm2600 <127270325+kmm2600@users.noreply.github.com>
2023-03-18 12:52:35 -0500

    Update streamlit_app.py
    add some more formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ team = list(df['RSO Team'].drop_duplicates())
 
 team_choice = st.selectbox('Filter on an RSO Team', team)
 
+st.subheader("The table below shows all players under contract in :{df.columns[9]:")
 df = df[df['RSO Team'] == team_choice]
 df = df[df.iloc[:,9] > 0]
 yr1_sum = "${:,d}".format(df.iloc[:,8].sum())
@@ -25,7 +26,8 @@ yr3_sum = "${:,d}".format(df.iloc[:,10].sum())
 yr4_sum = "${:,d}".format(df.iloc[:,11].sum())
 
 st.dataframe(df, use_container_width = True)
-
+st.caption("You can drag the lower-right corner to re-size the table")
+st.subheader("Below are the contracts totals by year")
 col1, col2, col3 = st.columns(3)
 col1.metric(df.columns[9], yr2_sum, delta = None)
 col2.metric(df.columns[10], yr3_sum, delta = None)
