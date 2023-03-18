bff975c1ad7c166974b8e91462cf66d1e212b123
kmm2600 <127270325+kmm2600@users.noreply.github.com>
2023-03-18 15:48:22 -0500

    Update streamlit_app.py
    download bug fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,11 +34,11 @@ st.dataframe(df, use_container_width = True)
 st.caption("You can drag the lower-right corner to re-size the table")
 
 @st.cache
-def convert_df(df):
+def convert_df(df2):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('utf-8')
+    return df2.to_csv().encode('utf-8')
 
-csv = convert_df(my_large_df)
+csv = convert_df(df)
 
 st.download_button(
     label="Download table as CSV",
