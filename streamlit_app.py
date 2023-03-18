3e49d67c091d11145e70dabfbd2179f34d2db09b
kmm2600 <127270325+kmm2600@users.noreply.github.com>
2023-03-18 12:56:37 -0500

    Update streamlit_app.py
    another rix

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,10 @@ df = load_data(st.secrets["public_gsheets_url"])
 st.title(f"RSO Team Contract Viewer")
 
 team = list(df['RSO Team'].drop_duplicates())
-
+curr_yr = df.columns[9]
 team_choice = st.selectbox('Filter on an RSO Team', team)
 
-st.subheader("The table below shows all players under contract in ", df.columns[9])
+st.subheader("The table below shows all players under contract in ", curr_yr)
 df = df[df['RSO Team'] == team_choice]
 df = df[df.iloc[:,9] > 0]
 yr1_sum = "${:,d}".format(df.iloc[:,8].sum())
