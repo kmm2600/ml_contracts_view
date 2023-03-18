e8c6ed4cb9173ad1f6abf4ce3693a33ff449bb19
kmm2600 <127270325+kmm2600@users.noreply.github.com>
2023-03-18 11:17:58 -0500

    Add aggrid options

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,4 +17,25 @@ df = load_data(st.secrets["public_gsheets_url"])
 #    st.write(f"{row.Player} has a :{row.Pos}:")
 
 #st.dataframe(df, use_container_width=True)
-AgGrid(df)
+gb = GridOptionsBuilder.from_dataframe(df)
+gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+gb.configure_side_bar() #Add a sidebar
+gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+gridOptions = gb.build()
+
+grid_response = AgGrid(
+    data,
+    gridOptions=gridOptions,
+    data_return_mode='AS_INPUT', 
+    update_mode='MODEL_CHANGED', 
+    fit_columns_on_grid_load=False,
+    theme='blue', #Add theme color to the table
+    enable_enterprise_modules=True,
+    height=350, 
+    width='100%',
+    reload_data=True
+)
+
+df = grid_response['data']
+selected = grid_response['selected_rows'] 
+df2 = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
